eef871744d70d2a7b9eb2cbf00dc0760d2cb43d1
hjq <13293349837@163.com>
2020-10-25 22:45:23 +0800

    添加头/脚

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { FootComponent } from './pages/foot/foot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FootComponent
   ],
   imports: [
     BrowserModule,
