d6e1774b6351da1197db539103f8b9e86c4cdc67
hjq <houjianqiao@datangmobile.cn>
2020-10-28 18:28:32 +0800

    添加ng-zorro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,18 @@ import { HeaderComponent } from './pages/header/header.component';
 import { FootComponent } from './pages/foot/foot.component';
 import { OblogComponent } from './pages/oblog/oblog.component';
 import { OblogModule } from './pages/oblog/oblog.module';
+import { IconsProviderModule } from './icons-provider.module';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -18,9 +30,15 @@ import { OblogModule } from './pages/oblog/oblog.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    OblogModule
+    OblogModule,
+    IconsProviderModule,
+    NzLayoutModule,
+    NzMenuModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
