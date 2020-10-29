a72f893752b839bfdf4e31a4f2560465116c6b20
hjq <houjianqiao@datangmobile.cn>
2020-10-29 18:10:22 +0800

    添加ng-zorro1

--- src/app/pages/oblog/oblog.module.ts
+++ src/app/pages/oblog/oblog.module.ts
@@ -3,13 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { OblogRoutingModule } from './oblog-routing.module';
 import {OblogComponent} from './oblog.component';
+import {NzBreadCrumbModule} from 'ng-zorro-antd/breadcrumb';
+import {NzLayoutModule} from 'ng-zorro-antd/layout';
+import {NzDropDownModule} from 'ng-zorro-antd/dropdown';
+import {NzMenuModule} from 'ng-zorro-antd/menu';
+import {NzIconModule} from 'ng-zorro-antd/icon';
 
 
 @NgModule({
   declarations: [OblogComponent],
   imports: [
     CommonModule,
-    OblogRoutingModule
+    OblogRoutingModule,
+    NzBreadCrumbModule,
+    NzLayoutModule,
+    NzDropDownModule,
+    NzMenuModule,
+    NzIconModule
   ],
   exports: [OblogComponent]
 })
